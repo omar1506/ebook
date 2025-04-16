ac5d5cdfafdd2e72646bbc9044ec95d1cfa4b968
omar1506 <zamoraherreraomarmissael@gmail.com>
2025-04-15 22:51:41 -0600

    Add files via upload

---
+++ PAGINA DE VENTAS/script.js
@@ -0,0 +1,7 @@
+const botonComprar = document.querySelector('button');
+
+botonComprar.addEventListener('click', () => {
+  // Redirigir a la página de pago de PayPal
+  window.location.href = 'https://go.hotmart.com/F98031316M';
+});
+
