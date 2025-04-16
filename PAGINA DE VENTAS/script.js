f7278021d7eb062e4db03533bc288ea7ffff5e4c
omar1506 <zamoraherreraomarmissael@gmail.com>
2025-04-16 09:41:48 -0600

    Update script.js

--- PAGINA DE VENTAS/script.js
+++ PAGINA DE VENTAS/script.js
@@ -2,6 +2,6 @@ const botonComprar = document.querySelector('button');
 
 botonComprar.addEventListener('click', () => {
   // Redirigir a la página de pago de PayPal
-  window.location.href = 'https://go.hotmart.com/F98031316M';
+  window.location.href = '';
 });
 
